4bbeedf510dad3a3284058bb4089254cd0d5a8f7
Tonysw2 <anthonyrmayaa@gmail.com>
2023-11-27 03:44:26 -0300

    feat: utilize useSafeAsyncAction to ensure actions only occur if the component is mounted

--- frontend/src/pages/EditContact/index.tsx
+++ frontend/src/pages/EditContact/index.tsx
@@ -4,32 +4,39 @@ import { ContactForm, ContactFormRef } from '../../components/ContactForm'
 import { Loader } from '../../components/Loader'
 import { PageHeader } from '../../components/PageHeader'
 import { ContactDTO } from '../../dtos/ContactDTO'
+import { useSafeAsyncAction } from '../../hooks/useSafeAsyncAction'
 import ContactsService from '../../services/ContactsService'
 import { toast } from '../../utils/toast'
 
 export function EditContact() {
-  const contactFormRef = useRef<ContactFormRef | null>(null)
+  const safeAsyncAction = useSafeAsyncAction()
   const [isLoading, setIsLoading] = useState(true)
   const [contactName, setContactName] = useState('')
+  const contactFormRef = useRef<ContactFormRef | null>(null)
 
   const { id } = useParams() as { id: string }
   const history = useHistory()
 
   useEffect(() => {
-    loadContact()
-  }, [id])
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id)
 
-  async function loadContact() {
-    try {
-      const contactData = await ContactsService.getContactById(id)
-      contactFormRef.current?.setCurrentFieldValues(contactData)
-      setIsLoading(false)
-      setContactName(contactData.name)
-    } catch {
-      history.push('/')
-      toast({ text: 'Contato não encontrado.', variant: 'danger' })
+        safeAsyncAction(() => {
+          contactFormRef.current?.setCurrentFieldValues(contactData)
+          setIsLoading(false)
+          setContactName(contactData.name)
+        })
+      } catch (error) {
+        safeAsyncAction(() => {
+          history.push('/')
+          toast({ text: 'Contato não encontrado.', variant: 'danger' })
+        })
+      }
     }
-  }
+
+    loadContact()
+  }, [])
 
   async function handleSubmit(
     formData: Omit<ContactDTO, 'id' | 'category_name'>,
